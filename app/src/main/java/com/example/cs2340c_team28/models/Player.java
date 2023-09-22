5a82338242db13bd4b8f234310122cfd8bc5f6a1
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2023-09-22 13:06:50 -0400

    Add Game, Player, Difficulty classes

---
+++ app/src/main/java/com/example/cs2340c_team28/models/Player.java
@@ -0,0 +1,84 @@
+package com.example.cs2340c_team28.models;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Class representing a player in the game
+ *
+ * @author Steven Baker
+ */
+public class Player {
+
+    /**
+     * The name of the player
+     */
+    private final String name;
+
+    /**
+     * The health points (hp) of the player
+     */
+    private int hp;
+
+    /**
+     * An integer id representing the player's sprite graphic,
+     *  avoids storing resource name in the data model
+     */
+    private final int spriteId;
+
+    /**
+     * Instantiate the player
+     * @param name The player's name
+     * @param spriteId Chosen sprite id
+     */
+    public Player(String name, int spriteId) {
+        this.name = name;
+        this.spriteId = spriteId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the player's health points
+     * @return The current health points for the player
+     */
+    public int getHp() {
+        return hp;
+    }
+
+    /**
+     * Set the player's health points
+     * @param hp The new value of health points to set
+     */
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    /**
+     * Get the player's sprite id
+     * @return The current sprite id for the player
+     */
+    public int getSpriteId() {
+        return spriteId;
+    }
+
+    /**
+     * Gets the ideal initial health points based on the given game difficulty
+     *
+     * @param difficulty The intended difficulty of the game
+     * @return The value of health points
+     */
+    public static int initialHp(@NonNull Difficulty difficulty) {
+        switch (difficulty) {
+            case EASY:
+                return 150;
+            case MEDIUM:
+                return 100;
+            case HARD:
+                return 50;
+        }
+        return -1;
+    }
+
+}
