4815d3d805aa30083b0298be2b522efb71e7a43f
Devin Burke <dburke44@gatech.edu>
2023-09-23 15:48:29 -0400

    Add button functionality for game screen to end screen, add health, player name, and difficulty to game screen

--- app/src/main/java/com/example/cs2340c_team28/GameScreen/GameActivity.java
+++ app/src/main/java/com/example/cs2340c_team28/GameScreen/GameActivity.java
@@ -1,18 +1,31 @@
 package com.example.cs2340c_team28.GameScreen;
 
-import android.app.Activity;
+import static com.example.cs2340c_team28.R.*;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
 
-public class GameActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(layout.activity_game);
+        TextView playerName = findViewById(id.playerName);
+        TextView playerHealth = findViewById(id.playerHealth);
+        TextView difficulty = findViewById(id.difficulty);
+        playerName.setText("Jerry");
+        playerHealth.setText("50/50");
+        difficulty.setText("HARD");
+        Button endScreenButton = findViewById(id.endScreenButton);
 
-        // TODO: use xml (activity_game.xml) instead of view class
-        setContentView(new GameView(this));
+        endScreenButton.setOnClickListener(view -> {
+            //Intent intent = new Intent(GameActivity.this, EndScreen.class);
+            //startActivity(intent);
+        });
     }
 }
