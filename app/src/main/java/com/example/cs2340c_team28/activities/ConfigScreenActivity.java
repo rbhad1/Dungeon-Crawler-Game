b5aa9a9d5595a9c22b08d787f49346043489b038
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2023-10-07 18:22:29 -0400

    Restructure config screen for MVVM paradigm

--- app/src/main/java/com/example/cs2340c_team28/activities/ConfigScreenActivity.java
+++ app/src/main/java/com/example/cs2340c_team28/activities/ConfigScreenActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,9 +13,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.cs2340c_team28.R;
-import com.example.cs2340c_team28.models.Difficulty;
-import com.example.cs2340c_team28.models.Game;
-import com.example.cs2340c_team28.models.Player;
+import com.example.cs2340c_team28.viewmodels.ConfigScreenViewModel;
 
 /**
  * Activity for game configuration screen.
@@ -38,22 +35,10 @@ public class ConfigScreenActivity extends AppCompatActivity {
     private ImageView[] spriteViews;
 
     private TextView startHint;
+
     private Button startGameButton;
 
-    /**
-     * The difficulty with which to start the game
-     */
-    private Difficulty difficulty;
-
-    /**
-     * The name entered by the player. This may or may not be valid.
-     */
-    private String playerName;
-
-    /**
-     * The index of the sprite the player wants to use
-     */
-    private int spriteIndex = 1;
+    private ConfigScreenViewModel viewModel;
 
     /**
      * Creates the view.
@@ -86,27 +71,28 @@ public class ConfigScreenActivity extends AppCompatActivity {
         spriteButtons = new Button[]{spriteButton1, spriteButton2, spriteButton3};
         spriteViews = new ImageView[]{spriteView1, spriteView2, spriteView3};
 
-        // Update view state
-        validateConfigState();
+        viewModel = new ConfigScreenViewModel(this);
+
+        viewModel.refreshViewsWithConfigState();
 
         // Listener for player name input
         playerNameEditText.addTextChangedListener(onPlayerNameTextChange);
 
         // Listener for difficulty select
-        radioGroup.setOnCheckedChangeListener(onDifficultySelect);
+        radioGroup.setOnCheckedChangeListener(viewModel::onDifficultyButtonClicked);
 
         // Listener for sprite select
         for (Button button: spriteButtons) {
-            button.setOnClickListener(onSpriteButtonClick);
+            button.setOnClickListener(viewModel::onSpriteButtonClicked);
         }
 
         // Listener for start game select
-        startGameButton.setOnClickListener(onStartGameButtonClick);
+        startGameButton.setOnClickListener(v -> viewModel.onStartGameButtonClicked());
     }
 
     /**
      * TextWatcher for the player name text field.
-     * Updates the {@link #playerName} variable with the contents entered into text field
+     * Forwards text field value to {@link ConfigScreenViewModel}
      */
     private final TextWatcher onPlayerNameTextChange = new TextWatcher() {
         @Override
@@ -121,69 +107,32 @@ public class ConfigScreenActivity extends AppCompatActivity {
 
         @Override
         public void afterTextChanged(Editable s) {
-            playerName = s.toString();
-            validateConfigState();
+            viewModel.playerNameTextChanged(s.toString());
         }
     };
 
-    /**
-     * Listener for the sprite selector buttons.
-     * Updates the {@link #spriteIndex} variable based on the selected button.
-     * Sets sprite ImageView opacity and button enable/disable flags.
-     */
-    private final View.OnClickListener onSpriteButtonClick = view -> {
-        for (int i = 0; i < spriteButtons.length; i++) {
-            boolean isSelectedSprite = spriteButtons[i].getId() == view.getId();
-
-            if (isSelectedSprite) {
-                spriteIndex = i + 1;
-            }
-
-            spriteButtons[i].setEnabled(!isSelectedSprite);
-            spriteViews[i].setAlpha(isSelectedSprite ? 1.0f : 0.2f);
-        }
-    };
-
-    /**
-     * Listener for the difficulty selector buttons.
-     * Updates the {@link #difficulty} variable based on the selected difficulty
-     */
-    private final RadioGroup.OnCheckedChangeListener onDifficultySelect = (group, checkedId) -> {
-        View radioButton = group.findViewById(checkedId);
-        int index = group.indexOfChild(radioButton);
-        difficulty = Difficulty.values()[index];
-        validateConfigState();
-    };
-
-    /**
-     * Listener for the start game button.
-     * It is assumed that if this button was pressed, the config params must have been valid
-     */
-    private final View.OnClickListener onStartGameButtonClick = view -> {
-        Game.createNewGame(difficulty);
-        Player.createNewPlayer(playerName, difficulty, spriteIndex);
-
+    public void openGameActivity() {
         Intent intent = new Intent(ConfigScreenActivity.this, GameActivity.class);
         startActivity(intent);
-    };
-
-    /**
-     * Function to validate whether what the user has selected can be used to start the game
-     */
-    private void validateConfigState() {
-
-        boolean playerNameValid = playerName != null && playerName.matches("[:alnum:].*");
-        playerNameEditText.setError(
-                playerNameValid ? null : "Player name must start with a letter or number.");
-
-        boolean difficultyValid = difficulty != null;
-
-        boolean canStart = playerNameValid && difficultyValid;
-
-        startHint.setVisibility(canStart ? View.INVISIBLE : View.VISIBLE);
-        startGameButton.setEnabled(canStart);
     }
 
+    public EditText getPlayerNameEditText() {
+        return playerNameEditText;
+    }
 
+    public Button[] getSpriteButtons() {
+        return spriteButtons;
+    }
 
+    public ImageView[] getSpriteViews() {
+        return spriteViews;
+    }
+
+    public TextView getStartHint() {
+        return startHint;
+    }
+
+    public Button getStartGameButton() {
+        return startGameButton;
+    }
 }
