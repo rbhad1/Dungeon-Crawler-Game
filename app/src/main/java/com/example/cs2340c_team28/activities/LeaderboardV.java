7a46dd1eb3c196bd1daa117f3052ac9c6aeca462
Riya Bhad <riyab2023@outlook.com>
2023-10-07 21:54:22 -0400

    Fix errors in code: variable names, modifiers, checkstyles

--- app/src/main/java/com/example/cs2340c_team28/activities/LeaderboardV.java
+++ app/src/main/java/com/example/cs2340c_team28/activities/LeaderboardV.java
@@ -1,5 +1,6 @@
 package com.example.cs2340c_team28.activities;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,9 +13,11 @@ import java.util.List;
 
 public class LeaderboardV extends Activity {
 
-    LeaderBoardVM leaderBoardVM = new LeaderBoardVM();
+    private final LeaderBoardVM leaderBoardVM = new LeaderBoardVM();
 
-    private List<Leaderboard.LeaderboardEntry> leaderboardEntries = leaderBoardVM.getLeaderboardEntries();
+    private final List<Leaderboard.LeaderboardEntry> leaderboardEntries
+            = leaderBoardVM.getLeaderboardEntries();
+    @SuppressLint("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -24,43 +27,42 @@ public class LeaderboardV extends Activity {
 
         if (Leaderboard.getInstance() == null) {
             entryOne.setText("Start playing to view the leaderboard!");
-        }
-        if (leaderboardEntries.size() < 5) {
+        } else if (leaderboardEntries.size() < 5) {
             entryOne = findViewById(R.id.entry1);
             entryOne.setText("More players need to play the game to view the leaderboard!");
-        }
-        else { entryOne = findViewById(R.id.entry1);
+        } else {
+            entryOne = findViewById(R.id.entry1);
             Leaderboard.LeaderboardEntry entry1 = leaderboardEntries.get(0);
-            entryOne.setText(leaderboardEntries.get(0).getDate() + ": " +
-                    leaderboardEntries.get(0).getPlayerName() + "   " +
-                    leaderboardEntries.get(0).getScore());
+            entryOne.setText(leaderboardEntries.get(0).getDate()
+                    + ": " + leaderboardEntries.get(0).getPlayerName() + "   "
+                    + leaderboardEntries.get(0).getScore());
 
 
             TextView entryTwo = findViewById(R.id.entry2);
             Leaderboard.LeaderboardEntry entry2 = leaderboardEntries.get(1);
-            entryOne.setText(leaderboardEntries.get(1).getDate() + ": " +
-                    leaderboardEntries.get(1).getPlayerName() + "   " +
-                    leaderboardEntries.get(1).getScore());
+            entryOne.setText(leaderboardEntries.get(1).getDate() + ": "
+                    + leaderboardEntries.get(1).getPlayerName() + "   "
+                    + leaderboardEntries.get(1).getScore());
 
             TextView entryThree = findViewById(R.id.entry3);
             Leaderboard.LeaderboardEntry entry3 = leaderboardEntries.get(2);
-            entryOne.setText(leaderboardEntries.get(2).getDate() + ": " +
-                    leaderboardEntries.get(2).getPlayerName() + "   " +
-                    leaderboardEntries.get(2).getScore());
+            entryOne.setText(leaderboardEntries.get(2).getDate() + ": "
+                    + leaderboardEntries.get(2).getPlayerName() + "   "
+                    + leaderboardEntries.get(2).getScore());
 
 
-            TextView entryFour = findViewById(R.id.entry3);
+            TextView entryFour = findViewById(R.id.entry4);
             Leaderboard.LeaderboardEntry entry4 = leaderboardEntries.get(3);
-            entryOne.setText(leaderboardEntries.get(3).getDate() + ": " +
-                    leaderboardEntries.get(3).getPlayerName() + "   " +
-                    leaderboardEntries.get(3).getScore());
+            entryOne.setText(leaderboardEntries.get(3).getDate() + ": "
+                    + leaderboardEntries.get(3).getPlayerName() + "   "
+                    + leaderboardEntries.get(3).getScore());
 
 
-            TextView entryFive = findViewById(R.id.entry1);
+            TextView entryFive = findViewById(R.id.entry5);
             Leaderboard.LeaderboardEntry entry5 = leaderboardEntries.get(4);
-            entryOne.setText(leaderboardEntries.get(4).getDate() + ": " +
-                    leaderboardEntries.get(4).getPlayerName() + "   " +
-                    leaderboardEntries.get(4).getScore());
+            entryOne.setText(leaderboardEntries.get(4).getDate() + ": "
+                    + leaderboardEntries.get(4).getPlayerName() + "   "
+                    + leaderboardEntries.get(4).getScore());
         }
 
     }
