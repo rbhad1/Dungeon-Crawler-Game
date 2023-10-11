77d7583cec1e6636e3414e2acc9ac69a5573aac3
Devin Burke <dburke44@gatech.edu>
2023-10-10 21:28:12 -0400

    Copy player sprites to assets and remove ScreenUtils.clear() in GameThread's render()

--- app/src/main/java/com/example/cs2340c_team28/threads/GameThread.java
+++ app/src/main/java/com/example/cs2340c_team28/threads/GameThread.java
@@ -3,7 +3,6 @@ package com.example.cs2340c_team28.threads;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.example.cs2340c_team28.activities.TiledView;
 import com.example.cs2340c_team28.models.Game;
 import com.example.cs2340c_team28.models.Player;
@@ -55,9 +54,8 @@ public class GameThread extends com.badlogic.gdx.Game {
     @Override
     public void render() {
         super.render();
-        ScreenUtils.clear(0, 0, 0, 0); //placeholder
         batch.begin();
-        batch.draw(playerImage, 0, 0); //placeholder
+        batch.draw(playerImage, 450, 600);
         batch.end();
         updateGameLogic();
     }
