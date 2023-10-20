d06e21cb3612f690b41656e43cb861b04be97e30
elijahmccauley <emmccauley6@gmail.com>
2023-10-20 14:25:54 -0400

    Add some very basic boundary collisions

--- app/src/main/java/com/example/cs2340c_team28/models/ConcreteMovement.java
+++ app/src/main/java/com/example/cs2340c_team28/models/ConcreteMovement.java
@@ -1,23 +1,33 @@
 package com.example.cs2340c_team28.models;
 
+import com.badlogic.gdx.Gdx;
+
 public class ConcreteMovement implements MovementStrategy {
     public void moveUp() {
         int y = Player.getUniquePlayerInstance().getY();
-        Player.getUniquePlayerInstance().setY(y + 32);
+        if (y + Gdx.graphics.getHeight() / 16 < Gdx.graphics.getHeight()) {
+            Player.getUniquePlayerInstance().setY(y + Gdx.graphics.getHeight() / 16);
+        }
     }
     @Override
     public void moveDown() {
         int y = Player.getUniquePlayerInstance().getY();
-        Player.getUniquePlayerInstance().setY(y - 32);
+        if (y - Gdx.graphics.getHeight() / 16 > 0) {
+            Player.getUniquePlayerInstance().setY(y - Gdx.graphics.getHeight() / 16);
+        }
     }
     @Override
     public void moveLeft() {
         int x = Player.getUniquePlayerInstance().getX();
-        Player.getUniquePlayerInstance().setX(x - 32);
+        if (x - Gdx.graphics.getWidth() / 9 > 0) {
+            Player.getUniquePlayerInstance().setX(x - Gdx.graphics.getWidth() / 9);
+        }
     }
     @Override
     public void moveRight() {
         int x = Player.getUniquePlayerInstance().getX();
-        Player.getUniquePlayerInstance().setX(x + 32);
+        if (x + Gdx.graphics.getWidth() / 9 < Gdx.graphics.getWidth()) {
+            Player.getUniquePlayerInstance().setX(x + Gdx.graphics.getWidth() / 9);
+        }
     }
 }
