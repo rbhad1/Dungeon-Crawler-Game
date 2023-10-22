3bc1292c1580fc07ef85a60f6eb0a209633a051d
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2023-10-22 17:02:24 -0400

    Change door handling to find door layer

--- app/src/main/java/com/example/cs2340c_team28/viewmodels/GameViewModel.java
+++ app/src/main/java/com/example/cs2340c_team28/viewmodels/GameViewModel.java
@@ -115,20 +115,17 @@ public class GameViewModel extends com.badlogic.gdx.Game {
             game.setScoreTime(currentTime);
         }
 
-        if (Game.getInstance().getCurrentMap().equals(forest)) {
-            if (player.getX(true) == 7 && player.getY(true) == 0) {
+        // Check if we've reached door
+        if (game.getDoorLayer()
+                .getCell(player.getX(true), player.getY(true))
+                .getTile()
+                .getId() != 0) {
+
+            if (game.getCurrentMap().equals(forest)) {
                 Game.getInstance().setCurrentMap(water);
-            }
-        }
-        if (Game.getInstance().getCurrentMap().equals(water)) {
-            if (player.getX(true) == 0 && player.getY(false) == 15 * 32) {
+            } else if (game.getCurrentMap().equals(water)) {
                 Game.getInstance().setCurrentMap(dungeon);
-            }
-        }
-        
-        if (Game.getInstance().getCurrentMap().equals(dungeon)) {
-            if (1 < player.getX(true) && player.getX(true) < 7
-                    && 0 < player.getY(true) && player.getY(true) < 12) {
+            } else if (game.getCurrentMap().equals(dungeon)) {
                 this.endGame();
             }
         }
