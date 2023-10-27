01004c37b643d10f328b67da0a7bd65f5eca82dc
Riya Bhad <riyab2023@outlook.com>
2023-10-27 13:44:54 -0400

    Implement factory method and create necessary classes and interface

---
+++ app/src/main/java/com/example/cs2340c_team28/models/EnemyHandler.java
@@ -0,0 +1,30 @@
+package com.example.cs2340c_team28.models;
+
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+
+abstract class EnemyHandler {
+    Enemy enemy;
+
+    void initialize() {
+        Game game = Game.getInstance();
+        TiledMap currentMap = game.getCurrentMap();
+
+        if (currentMap.getProperties().containsKey("forest")) {
+            enemy = new GroundEnemy();
+            enemy = new AirEnemy();
+        } else if (currentMap.getProperties().containsKey("water")) {
+            enemy = new WaterEnemy();
+            enemy = new AirEnemy();
+        } else if (currentMap.getProperties().containsKey("portal")) {
+            enemy = new GroundEnemy();
+            enemy = new FireEnemy();
+
+        }
+    }
+    void main() {
+        this.initialize();
+    }
+
+
+}
