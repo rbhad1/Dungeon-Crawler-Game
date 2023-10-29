ca29f792b47b4377e0055393cef33b67b6d8e9c1
Riya Bhad <riyab2023@outlook.com>
2023-10-29 16:56:07 -0400

    Implement render method

--- app/src/main/java/com/example/cs2340c_team28/models/WaterEnemy.java
+++ app/src/main/java/com/example/cs2340c_team28/models/WaterEnemy.java
@@ -1,4 +1,35 @@
 package com.example.cs2340c_team28.models;
 
-public class WaterEnemy extends Enemy {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class WaterEnemy extends Movable implements Enemy {
+    private int spriteId;
+    private Texture enemyImage;
+
+    private Sprite sprite;
+
+    private SpriteBatch batch;
+    String imageResource;
+    public WaterEnemy() {
+
+    }
+
+
+
+    // TODO: update name of file when sprites are ready
+    @Override
+    public void render() {
+        imageResource = null;
+        enemyImage = new Texture(imageResource);
+        batch = new SpriteBatch();
+        batch.begin();
+        batch.draw(enemyImage,32, 32);
+        batch.end();
+
+    }
+    public int getSpriteId() {
+        return spriteId;
+    }
 }
