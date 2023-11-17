237b004c47b81a77b390d3a9db816700d0b92c51
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2023-11-17 12:46:03 -0500

    Create new Positionable class from which PowerUp extends

--- app/src/main/java/com/example/cs2340c_team28/models/Movable.java
+++ app/src/main/java/com/example/cs2340c_team28/models/Movable.java
@@ -3,67 +3,8 @@ package com.example.cs2340c_team28.models;
 import com.example.cs2340c_team28.models.movement.Movement;
 import com.example.cs2340c_team28.models.movement.Position;
 
-public abstract class Movable {
+public abstract class Movable extends Positionable {
     private Movement currentMovement;
-
-    private int x;
-    private int y;
-
-    /**
-     * Get the current x position
-     * @param tileRelative If true, give position as number of tiles
-     * @return x position
-     */
-    public int getX(boolean tileRelative) {
-        return this.x / (tileRelative ? 32 : 1);
-    }
-
-    /**
-     * Get the current y position
-     * @param tileRelative If true, give position as number of tiles
-     * @return y position
-     */
-    public int getY(boolean tileRelative) {
-        return this.y / (tileRelative ? 32 : 1);
-    }
-
-    /**
-     * Get the current x position
-     * @param x The new position
-     * @param tileRelative If true, input is number of tiles rather than points
-     */
-    public void setX(int x, boolean tileRelative) {
-        this.x = x * (tileRelative ? 32 : 1);
-    }
-
-    /**
-     * Get the current y position
-     * @param y The new position
-     * @param tileRelative If true, input is number of tiles rather than points
-     */
-    public void setY(int y, boolean tileRelative) {
-        this.y = y * (tileRelative ? 32 : 1);
-    }
-
-    /**
-     * Get the current position as a Position object
-     * @param tileRelative If true, give position as number of tiles
-     * @return Current position
-     */
-    public Position getPosition(boolean tileRelative) {
-        Position position = new Position(this.x, this.y);
-        return tileRelative ? position.graphicalToTile() : position;
-    }
-
-    /**
-     * Get the current position as a Position object
-     * @param position The position to set
-     * @param tileRelative If true, give position as number of tiles
-     */
-    public void setPosition(Position position, boolean tileRelative) {
-        this.setX(position.getX(), tileRelative);
-        this.setY(position.getY(), tileRelative);
-    }
 
     public Movement getCurrentMovement() {
         return currentMovement;
--- app/src/main/java/com/example/cs2340c_team28/models/powerup/PowerUp.java
+++ app/src/main/java/com/example/cs2340c_team28/models/powerup/PowerUp.java
@@ -1,6 +1,10 @@
 package com.example.cs2340c_team28.models.powerup;
 
-public abstract class PowerUp {
+import com.badlogic.gdx.graphics.Texture;
+import com.example.cs2340c_team28.models.Positionable;
+
+public abstract class PowerUp extends Positionable {
+
     /**
      * A wrapped powerup to comply with the decorator pattern
      */
@@ -17,15 +21,21 @@ public abstract class PowerUp {
     private final long duration;
 
     /**
+     * The texture corresponding to the powerup
+     */
+    private final Texture texture;
+
+    /**
      * Construct the powerup
      * @param wrapped The wrapped powerup
      * @param startTime The start time
      * @param duration The duration
      */
-    public PowerUp(PowerUp wrapped, long startTime, long duration) {
+    public PowerUp(PowerUp wrapped, long startTime, long duration, Texture texture) {
         this.wrapped = wrapped;
         this.startTime = startTime;
         this.duration = duration;
+        this.texture = texture;
     }
 
     /**
@@ -61,5 +71,9 @@ public abstract class PowerUp {
 
     public long getDuration() {
         return duration;
+    }
+
+    public Texture getTexture() {
+        return texture;
     }
 }
