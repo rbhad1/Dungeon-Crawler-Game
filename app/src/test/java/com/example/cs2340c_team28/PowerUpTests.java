f8dfd2108bab4a63a4f5d46f855b34a8097ee28c
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2023-11-28 21:12:47 -0500

    Write new speed tests

--- app/src/test/java/com/example/cs2340c_team28/PowerUpTests.java
+++ app/src/test/java/com/example/cs2340c_team28/PowerUpTests.java
@@ -8,18 +8,16 @@ import static org.junit.Assert.assertTrue;
 import com.example.cs2340c_team28.helpers.GameViewModelTester;
 import com.example.cs2340c_team28.helpers.LibGdxTester;
 import com.example.cs2340c_team28.models.Game;
-import com.example.cs2340c_team28.models.Movable;
 import com.example.cs2340c_team28.models.Player;
 import com.example.cs2340c_team28.models.movement.Movement;
 import com.example.cs2340c_team28.models.movement.Position;
 import com.example.cs2340c_team28.models.movement.TileMovementStrategy;
-import com.example.cs2340c_team28.models.powerup.PowerUpDecorator;
-import com.example.cs2340c_team28.viewmodels.GameViewModel;
+import com.example.cs2340c_team28.models.powerup.PowerUp;
+import com.example.cs2340c_team28.models.powerup.SuperSpeedDecorator;
 
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * Unit tests for the config screen
@@ -39,28 +37,21 @@ public class PowerUpTests {
         Player player = Player.getInstance();
         Game game = Game.getInstance();
         game.setCurrentMap(gameViewModel.getForest());
-        Movable movable = Player.getInstance();
 
+        player.setPosition(new Position(0, 0), true);
+        Movement movement = new Movement(player.getPosition(true), new Position(1, 1), true, 200);
+        player.setCurrentMovement(movement);
 
-        Method handleMovement = GameViewModel.class.getDeclaredMethod("handleMovement", Movable.class);
-        handleMovement.setAccessible(true);
+        gameViewModel.cycledUpdate(4, 1);
 
-        movable.setPosition(new Position(0, 0), true);
-        Movement movement = new Movement(movable.getPosition(true), new Position(1, 1), true, 200);
-        movable.setCurrentMovement(movement);
+        long originalMovementDuration = player.getCurrentMovement().getDuration();
 
-        handleMovement.invoke(gameViewModel, movable);
+        player.setPowerUp(new SuperSpeedDecorator());
+        gameViewModel.cycledUpdate(4, 1);
 
-        long playerDuration = player.getCurrentMovement().getDuration();
+        long newMovementDuration = player.getCurrentMovement().getDuration();
 
-
-        handleMovement.invoke(gameViewModel, movable);
-
-        PowerUpDecorator powerUpDecorator = new PowerUpDecorator(new SuperSpeedTester());
-        powerUpDecorator.activate();
-        long newDuration = player.getCurrentMovement().getDuration();
-
-        assertNotEquals(playerDuration, newDuration);
+        assertNotEquals(originalMovementDuration, newMovementDuration);
 
     }
 
@@ -73,11 +64,38 @@ public class PowerUpTests {
         Game game = Game.getInstance();
         game.setCurrentMap(gameViewModel.getForest());
 
-        long originalMoveDuration = TileMovementStrategy.MOVE_DURATION;
-        SuperSpeedTester speed = new SuperSpeedTester();
-        speed.deactivate();
-        assertEquals(originalMoveDuration, TileMovementStrategy.MOVE_DURATION);
+        // Set the powerup
+        PowerUp powerUp = new SuperSpeedDecorator();
+        powerUp.setDuration(400);
+        player.setPowerUp(powerUp);
 
-    }
+        // Do the initial movement
+        player.setPosition(new Position(2, 9), true);
+        Movement movement = new Movement(player.getPosition(true), new Position(3, 9), true, 200);
+        player.setCurrentMovement(movement);
+        long originalMovementDuration = player.getCurrentMovement().getDuration();
+
+        // This should update the movement duration
+        gameViewModel.cycledUpdate(4, 1);
+        long newMovementDuration = player.getCurrentMovement().getDuration();
+
+        // Make sure the movement duration updated
+        assertNotEquals(originalMovementDuration, newMovementDuration);
+
+        // Now increment time so that the movement finishes AND power-up expires
+        gameViewModel.cycledUpdate(10, 50);
+
+        // Set another movement
+        movement = new Movement(player.getPosition(true), new Position(2, 9), true, 200);
+        player.setCurrentMovement(movement);
+        originalMovementDuration = movement.getDuration();
+
+        gameViewModel.cycledUpdate(4, 1);
+
+        // Make sure the movement duration isn't changed this time around, since power-up ended
+        newMovementDuration = player.getCurrentMovement().getDuration();
+        assertEquals(originalMovementDuration, newMovementDuration);
+        gameViewModel.cycledUpdate(10, 50);
+            }
 
 }
